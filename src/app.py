ee893f6c4f2221a08a0d7ea952d1ec680b7c4567
Eric Au <ericdotau@gmail.com>
2023-05-13 23:17:46 -0700

    feat: streamlit app page

---
+++ src/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import streamlit as st
+
+page_title_input = 'Mammoth Bio Science Dev'
+
+
+def app():
+    st.markdown = page_title_input
+    st.sidebar.markdown(page_title_input)
+
+
+if __name__ == '__main__':
+    app()
---
+++ src/pages/page.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import streamlit as st
+
+page_title_input = 'Mammoth Bio Science Dev'
+
+
+def main():
+    st.markdown = page_title_input
+    st.sidebar.markdown(page_title_input)
+
+
+if __name__ == '__main__':
+    main()
